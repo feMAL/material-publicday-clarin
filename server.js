1f663c44756e4bae2ce146d3949e7dec1bcbbec7
FerMal <falgorm@gmail.com>
2020-07-28 00:29:24 -0300

    update to deploy in heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path    = require('path');
+const app     = express();
+
+app.use(express.static(__dirname + '/dist/public-days'));
+app.get('/*', ( req, res )=>{
+    res.sendFile(path.join(__dirname + '/dist/public-days/src/index.html'));
+});
+app.listen(process.env.PORT || 8080);
